372fcb5389823ddb74e2c3cba2dee957ab3c8dbe
T. Robert Ward <trironyard@icloud.com>
2016-07-18 15:05:29 -0400

    addition

--- currency.py
+++ currency.py
@@ -3,5 +3,15 @@ class Currency:
         self.amount = amount
         self.currency_code = currency_code
 
+
     def __eq__(self, other_stuff):
         return self.amount == other_stuff.amount and self.currency_code == other_stuff.currency_code
+
+
+    def __add__(self, other_amt):
+        if self.currency_code == other_amt.currency_code:
+            return (self.amount + other_amt.amount, self.currency_code)
+
+
+    # def atsnt(num1, num1)
+    #     reutrn n1=n2
--- test_currency.py
+++ test_currency.py
@@ -19,3 +19,9 @@ def test_currency_is_not_equal():
     currency2 = Currency(10, 'USD')
 
     assert currency1 != currency2
+
+def test_add_currency():
+    currency1 = Currency(11, 'USD')
+    currency2 = Currency(22, 'USD')
+
+    assert currency1 + currency2 == (33, 'USD')
