c5a90e2e99bfe432745f8486367a2f2a3730e959
T. Robert Ward <trironyard@icloud.com>
2016-07-18 14:49:54 -0400

    equal & not equal

--- test_currency.py
+++ test_currency.py
@@ -1,11 +1,21 @@
-#
+#W2D1 normal
+
+from currency import Currency
+
+def c_amount_and_c_code():
+    dollar = Currency(1, 'USD')
+    assert dollar == (1, 'USD')
+
 
 def test_currency_is_equal():
-    currency1 = (10, USD)
-    currency2 = (10, USD)
+    currency1 = Currency(10, 'USD')
+    currency2 = Currency(10, 'USD')
+
     assert currency1 == currency2
 
-    def test_currency_is_not_equal():
-        currency1 = (20, USD)
-        currency2 = (10, USD)
-        assert currency1 != currency2
+
+def test_currency_is_not_equal():
+    currency1 = Currency(20, 'USD')
+    currency2 = Currency(10, 'USD')
+
+    assert currency1 != currency2
